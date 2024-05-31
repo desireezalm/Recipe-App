688922366522b6a9dd387425dd4ea1873b899666
Desiree Zalm <82588366+desireezalm@users.noreply.github.com>
2024-05-31 14:32:32 +0200

    made minor changes to styling of app.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { Center, Heading } from "@chakra-ui/react";
+import { Center, Heading, Stack } from "@chakra-ui/react";
 import { RecipeListPage } from "./pages/RecipeListPage";
 import { RecipePage } from "./pages/RecipePage";
 import { useState } from "react";
 
 export const App = () => {
   const [selectedRecipe, setSelectedRecipe] = useState();
-  const pageTitle = "Welcome to the Recipe Hoarder";
+  const pageTitle = "Recipe Hoarder";
   return (
-    <Center>
+    <Center bgColor="teal.100" flexDir="column">
       {selectedRecipe ? (
         <RecipePage item={selectedRecipe} clickFn={setSelectedRecipe} />
       ) : (
         <>
-          <Heading size="2xl" mb={8} color="teal.600">
-            {pageTitle}
-          </Heading>
-
-          <RecipeListPage clickFn={setSelectedRecipe} />
+          <Stack alignItems="center">
+            <Heading size="xl" mb={8} color="teal.600">
+              {pageTitle}
+            </Heading>
+            <RecipeListPage clickFn={setSelectedRecipe} />
+          </Stack>
         </>
       )}
     </Center>
