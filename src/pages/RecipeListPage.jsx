72589bcb6786c069b114f0bfe70c4b6dbfff97bf
Desiree Zalm <82588366+desireezalm@users.noreply.github.com>
2024-05-31 14:30:20 +0200

    corrected code in RecipeListPage.jsx

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,9 +1,31 @@
-import { Center, Heading } from "@chakra-ui/react";
+import { Center, Flex, Text } from "@chakra-ui/react";
 import { data } from "../utils/data";
+import { RecipeCard } from "../components/RecipeCard";
 
-export const RecipeListPage = () => {
-  // You can play around with the console log, but ultimately remove it once you are done
-  console.log(data.hits[0].recipe.label);
+export const RecipeListPage = ({ clickFn }) => {
+  const recipeList = data.hits;
+  console.log(recipeList);
 
-  return <Center h="100vh" flexDir="column"></Center>;
+  return (
+    <Center flexDir="column">
+      <Flex
+        gap={8}
+        w="100%"
+        h="100%"
+        flexDirection={{ base: "column", sm: "row" }}
+        justify="center"
+        alignItems="center"
+        wrap="wrap"
+        padding="2rem"
+      >
+        {recipeList.map((item) => (
+          <RecipeCard
+            clickFn={clickFn}
+            item={item.recipe}
+            key={item.recipe.label}
+          />
+        ))}
+      </Flex>
+    </Center>
+  );
 };
