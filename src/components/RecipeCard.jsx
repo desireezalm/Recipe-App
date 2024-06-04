94a8434f37aa141328b28eb7cf993007c4bdd767
Desiree Zalm <82588366+desireezalm@users.noreply.github.com>
2024-06-04 15:03:28 +0200

    Made changes to styling

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,30 +1,44 @@
 import {
-  Box,
   Card,
   CardBody,
+  Divider,
   Flex,
   Heading,
   Image,
+  Spacer,
   Stack,
   Text,
 } from "@chakra-ui/react";
 
+import { TagItem } from "./ui/TagItem";
+
 export const RecipeCard = ({ item, clickFn }) => {
   console.log(item[0]);
+
+  const healthLabelQty = item.healthLabels.length;
+  const dietLabelQty = item.dietLabels.length;
+  const cautionLabelQty = item.cautions.length;
+  const healthLabelArray = item.healthLabels;
+
+  const croppedHealthLabels = healthLabelArray.filter((labelItem) => {
+    if (labelItem == "Vegetarian" || labelItem == "Vegan") {
+      return labelItem;
+    }
+  });
 
   return (
     <Card
       borderRadius="xl"
       cursor="pointer"
       bgColor="teal.200"
-      w="20rem"
-      h="fit-content"
+      w={{ base: "100%", sm: "22rem" }}
+      h={{ base: "fit-content", sm: "39rem" }}
       onClick={() => clickFn(item.recipe)}
       _hover={{ bgColor: "teal.300" }}
     >
       <Image
-        w="20rem"
-        h="20rem"
+        w={{ base: "100%", sm: "22rem" }}
+        h="22rem"
         objectFit="cover"
         src={item.image}
         borderTopRadius="xl"
@@ -49,12 +63,73 @@ export const RecipeCard = ({ item, clickFn }) => {
             {item.label}
           </Heading>
 
-          <Text fontSize={{ base: "sm", sm: "md" }}>
-            <Text display="inline-block" fontWeight="bold">
-              Dish:{" "}
+          <Text
+            fontSize={{ base: "sm", sm: "md" }}
+            color="teal.700"
+            fontWeight="light"
+          >
+            <Text display="inline-block" fontWeight="medium" color="teal.700">
+              dish:{" "}
             </Text>{" "}
             {item.dishType}
           </Text>
+
+          <Spacer></Spacer>
+          <Divider
+            color="teal.100"
+            orientation="horizontal"
+            w="12rem"
+            alignSelf="center"
+          ></Divider>
+          <Spacer></Spacer>
+
+          {healthLabelQty > 0 && (
+            <Flex
+              gap={2}
+              wrap="wrap"
+              fontSize={{ base: "sm", sm: "md" }}
+              color="teal.700"
+              fontWeight="light"
+            >
+              {croppedHealthLabels.map((label) => (
+                <TagItem key={label}>{label}s</TagItem>
+              ))}
+            </Flex>
+          )}
+
+          {dietLabelQty > 0 && (
+            <Flex
+              gap={2}
+              wrap="wrap"
+              fontSize={{ base: "sm", sm: "md" }}
+              color="teal.700"
+              fontWeight="light"
+            >
+              <Text display="inline-block" fontWeight="medium" color="teal.700">
+                diet:
+              </Text>{" "}
+              {item.dietLabels.map((dietLabel) => (
+                <TagItem key={dietLabel}>{dietLabel}</TagItem>
+              ))}
+            </Flex>
+          )}
+
+          {cautionLabelQty > 0 && (
+            <Flex
+              gap={2}
+              wrap="wrap"
+              fontSize={{ base: "sm", sm: "md" }}
+              color="teal.700"
+              fontWeight="light"
+            >
+              <Text display="inline-block" fontWeight="medium" color="teal.700">
+                caution:
+              </Text>{" "}
+              {item.cautions.map((caution) => (
+                <TagItem key={caution}>{caution}</TagItem>
+              ))}
+            </Flex>
+          )}
         </Stack>
       </CardBody>
     </Card>
--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -4,7 +4,7 @@ import { RecipeCard } from "../components/RecipeCard";
 
 export const RecipeListPage = ({ clickFn }) => {
   const recipeList = data.hits;
-  console.log(recipeList);
+  console.log(recipeList[0]);
 
   return (
     <Center flexDir="column">
@@ -16,7 +16,7 @@ export const RecipeListPage = ({ clickFn }) => {
         justify="center"
         alignItems="center"
         wrap="wrap"
-        padding="2rem"
+        padding={{ base: 0, sm: "2rem" }}
       >
         {recipeList.map((item) => (
           <RecipeCard
